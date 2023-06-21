359e018c5ada24b13c8718dd59640adb92a97d3b
joan <joan.brambila@gmail.com>
2023-06-20 21:16:14 -0700

    save changes

---
+++ develop/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const api = require('./public/assets/js/index.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+
+
+app.use(express.json());
+
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
